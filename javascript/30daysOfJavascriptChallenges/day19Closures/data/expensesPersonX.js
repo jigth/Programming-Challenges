6936c4ad7335287c647b6941a03741e32302a7a3
jigth <jigth>
2020-10-07 16:35:52 -0500

    Completing day 19, renaming folders for day 21

---
+++ javascript/30daysOfJavascriptChallenges/day19Closures/data/expensesPersonX.js
@@ -0,0 +1,14 @@
+const expenses = [
+    { 
+        "subject": "Buying basic equipment for school",
+        "value": 20
+    },
+    { 
+        "subject": "Subject 2",
+        "value": 15
+    },
+    { 
+        "subject": "Subject 3",
+        "value": 10
+    }
+]
---
+++ javascript/30daysOfJavascriptChallenges/day19Closures/data/incomesPersonX.js
@@ -0,0 +1,14 @@
+const incomes = [
+    { 
+        "subject": "Work 1",
+        "value": 70
+    },
+    { 
+        "subject": "Business 12",
+        "value": 70
+    },
+    { 
+        "subject": "Challenge 417",
+        "value": 65
+    }
+]
